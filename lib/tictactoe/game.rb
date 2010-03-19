794a5a2dc617e2d15732afabbaa5bb5313fd30e1
Craig Demyanovich <cdemyanovich@mutuallyhuman.com>
2010-03-18 22:49:10 -0400

    Game announces winner

--- lib/tictactoe/game.rb
+++ lib/tictactoe/game.rb
@@ -16,5 +16,15 @@ module TicTacToe
     def mark(square, piece)
       @board.mark(square, piece)
     end
+    
+    def announce_winner(winner)
+      if winner == "X"
+        @output.puts "X wins. Looks like I need to work on the 'un' in 'unbeatable'. :-/"
+      elsif winner == "O"
+        @output.puts "O wins! I'm unbeatable!"
+      elsif winner == " "
+        @output.puts "Draw. Let's play again."
+      end
+    end
   end
 end
--- spec/tictactoe/game_spec.rb
+++ spec/tictactoe/game_spec.rb
@@ -44,4 +44,29 @@ module TicTacToe
     end
     
   end
+  
+  describe Game, '#announce_winner' do
+    
+    before(:each) do
+      @board = double('board').as_null_object
+      @output = double('output').as_null_object
+      @game = Game.new(@board, @output)
+    end
+    
+    it "shamefully admits defeat if X wins" do
+      @output.should_receive(:puts).with("X wins. Looks like I need to work on the 'un' in 'unbeatable'. :-/")
+      @game.announce_winner("X")
+    end
+    
+    it "boastfully declares victory if O wins" do
+      @output.should_receive(:puts).with("O wins! I'm unbeatable!")
+      @game.announce_winner("O")
+    end
+    
+    it "casually notes a tie" do
+      @output.should_receive(:puts).with("Draw. Let's play again.")
+      @game.announce_winner(" ")
+    end
+    
+  end
 end
